c5da5befea0a0d7a1d72cfaf9fcd3c9ebbd9977c
liazz577 <liamcritlance@gmail.com>
2022-12-12 10:46:45 +0700

    prototype 1.0

---
+++ evs.js
@@ -0,0 +1,17 @@
+export const fbConfig = {
+    apiKey: "AIzaSyCiUl5ryauwZ8LzZ1rRbeCUQoIz_U1H_oU",
+    authDomain: "voice-video-chat-app-7b6af.firebaseapp.com",
+    databaseURL: "https://voice-video-chat-app-7b6af-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "voice-video-chat-app-7b6af",
+    storageBucket: "voice-video-chat-app-7b6af.appspot.com",
+    messagingSenderId: "107509003247",
+    appId: "1:107509003247:web:13cecc2c9ab7647c60bc1a",
+    measurementId: "G-KV3SBH3EGY"
+  };
+
+export const cometChatConfig = {
+    cometChatAppId: "2275416a9c7880fd",
+    cometChatRegion: "us",
+    cometChatAuthKey: "885dae4c768633200598cc422db9b1d04e8ea161",
+    cometChatRestApiKey: "446bad61d3b06bb4050b059c951ca0eb4e55eb9b"
+  };
